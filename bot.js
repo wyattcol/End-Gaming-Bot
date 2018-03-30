8abeef1f8ddb3a680b0f553dc6a1f7c32145e1b2
wyattcol <35353860+wyattcol@users.noreply.github.com>
2018-03-30 10:23:45 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,18 +57,26 @@ message.channel.send({embed: {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "Here is the latest video by End Gaming!",
-    url: " ",
-    description: "Make sure to like and subscribe!",
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
     fields: [{
-        name: "Video Link",
-        value: VideoLink
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
       }
     ],
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "Bot created and managed by Orangedude4221"
+      text: "© Example"
     }
   }
 });
